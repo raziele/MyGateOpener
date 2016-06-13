c288ac666d38f02053e53426b0bb089f052246e7
Raziel Einhorn <raziele@gmail.com>
2016-06-13 22:27:08 +0300

    .

--- Python/organizing_received_code.py
+++ Python/organizing_received_code.py
@@ -3,7 +3,10 @@
 import os
 import numpy as np
 
-fpath = "/home/maayan4/ParkingRC/parkRC_bytes"
+fpath = "/Users/raziele/Downloads/ParkingRCanalysis/parkRC_bytes" #"/home/maayan4/ParkingRC/parkRC_bytes"
+
+MAX_NUM_OF_SIGNALS = 15  #maximum number of signals in a file
+SZ_SIGNAL          = 161 #size in bits
 
 with open(fpath,'rb') as f:
     data = f.read()
@@ -12,6 +15,10 @@ data_array = np.fromstring(data, dtype='b')
 
 count = 0
 num_of_signals = 0
+
+preamble_code = np.array([1,1,1,1,1])
+
+Signals = np.zeros()
 
 for bit in data_array:
     if bit == 1:
